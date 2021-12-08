debd6320c62c6e069567422f7a4aa9b1d7875627
Timothy Small <smalls.online@icloud.com>
2021-12-08 13:42:06 -0500

    Added SimpleListGroup and SimpleListGroupItem

---
+++ src/SmallsOnline.Blazor.Components/components/list-groups/SimpleListGroupItem.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SmallsOnline.Blazor.Components.ListGroups
+{
+    public partial class SimpleListGroupItem : BootstrapComponent
+    {
+        protected override void OnParametersSet()
+        {
+            AddClassNamesToClassList();
+        }
+    }
+}
